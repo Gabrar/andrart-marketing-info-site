aa046b0c7ab417aa75041294961bfd824fa0a181
Gabrar <gabriel2025diasss@gmail.com>
2026-01-13 08:54:42 -0300

    responsive adjusts

--- src/App.jsx
+++ src/App.jsx
@@ -24,21 +24,21 @@ function App() {
         </header>
 
         <main className="py-5 flex m-6 lg:flex-row items-center bg-gra">
-          <div className="flex flex-col lg:w-full font-montserrat justify-center mb-auto xl:text-7xl xl:mb-auto lg:text-5xl md:text-4xl md:mb-auto lg:mb-auto xl:m-6 animate-fade-in">
+          <div className="flex flex-col lg:w-full font-montserrat justify-center sm:text-3xl mb-auto xl:text-6xl xl:mb-auto lg:text-5xl md:text-4xl md:mb-auto lg:mb-auto xl:m-6 animate-fade-in">
             <span className="tracking-widest animate-right">
               Agência <span className="text-[#e37035]">criativa </span> <br />e estratégica
             </span>
             
-            <span className="tracking-widest text-xs xl:text-3xl lg:text-2xl md:text-base animate-right">
-              Para quem quer ser <br className="xl:hidden lg:hidden md:hidden" />lembrado e ser visto.
+            <span className="tracking-widest text-xs sm:text-base xl:text-3xl lg:text-2xl md:text-base animate-right">
+              Para quem quer ser <br className="xl:hidden lg:hidden md:hidden" />visto e lembrado.
             </span>
 
-            <div className="hidden lg:block w-full md:max-w-4xl lg:max-w-3xl mt-12">
+            <div className="hidden lg:block w-full md:max-w-4xl lg:max-w-xl xl:max-w-3xl 2xl:max-w-4xl mt-20">
               <InfitieCarousel />
             </div>
 
             <div className="block lg:hidden mr-4"> 
-              <button className="text-xs bg-linear-to-r from-[#c73433] to-[#f3a73a] rounded-lg p-2 text-white whitespace-nowrap my-10 animate-right">
+              <button className="text-xs sm:text-base md:text-xl bg-linear-to-r from-[#c73433] to-[#f3a73a] rounded-lg p-2 text-white whitespace-nowrap my-10 animate-right">
                 ENTRE EM CONTATO
               </button>
             </div>
@@ -46,11 +46,11 @@ function App() {
           </div>
 
             
-              <div className="animate-fade-in ml-auto lg:w-full flex justify-center lg:ml-0">
+              <div className="animate-fade-in ml-auto lg:w-full flex justify-end lg:ml-0">
 
                 <div className="animate-right">
                   <div className="relative bg-linear-to-r from-[#c73433] via-[#e37035] to-[#f3a73a] rounded-2xl">
-                    <img src="assets/andre.png" className="relative h-50 w-auto xl:h-127 lg:h-100 md:h-90 object-cover" />
+                    <img src="assets/andre.png" className="relative h-50 w-auto xl:h-127 lg:h-100 md:h-90 sm:h-70 object-cover" />
                     <div className="absolute inset-0 bg-linear-to-b from-transparent via-black/20 to-black h-full w-full"></div>
                   </div>
 
